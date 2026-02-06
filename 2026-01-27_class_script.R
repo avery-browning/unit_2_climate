bfbfeef36d5de2305a3702d8648d69c1991095a3
avery-browning <avery.o.browning@gmail.com>
2026-02-06 11:24:42 -0500

    update class notes

--- 2026-01-27_class_script.R
+++ 2026-01-27_class_script.R
@@ -1,10 +1,10 @@
 # 2026-01-27 ; class script
 
 # read in data - using the relative path
-ant_ice_loss = read.table(file = "data/antarctica_mass_200204_202505.txt",
+ant_ice_loss = read.table("unit_2_climate/data/antarctica_mass_200204_202505.txt",
                           skip=31, header=F, col.names= c("decimal_date", "mass_Gt", "sigma_Gt"))
 ant_ice_loss
-grn_ice_loss = read.table(file = "data/greenland_mass_200204_202505.txt",
+grn_ice_loss = read.table("unit_2_climate/data/greenland_mass_200204_202505.txt",
                           skip=31, header=F, col.names= c("decimal_date", "mass_Gt", "sigma_Gt"))
 head(grn_ice_loss)
 #head prints out the first 6 rows
@@ -17,6 +17,8 @@ range(grn_ice_loss$mass_Gt)
 plot(x=ant_ice_loss$decimal_date, y=ant_ice_loss$mass_Gt,
     ylim = range(grn_ice_loss$mass_Gt),
     type="l", xlab="", ylab="Ice mass loss (Gt)")
+
+#plot mass_Gt as a function of decimal_date -> formula y ~ x
 
 lines(mass_Gt ~ decimal_date,
     data=grn_ice_loss,
@@ -42,20 +44,44 @@ tail(ant_ice_loss_NA)
 grn_ice_loss_NA = rbind(grn_ice_loss, data_break)
 grn_ice_loss_NA = grn_ice_loss_NA[order(grn_ice_loss_NA$decimal_date), ]
 
+# can use + to link multiple plots together
 plot(mass_Gt ~ decimal_date,
     data= ant_ice_loss_NA,  
-    ylim = range(grn_ice_loss$mass_Gt),
-    type="l", xlab="", ylab="")
+    ylim = range(grn_ice_loss$mass_Gt, na.rm=TRUE),
+    type="l", xlab="", ylab="") +
 
-# review class recording here - missed some lines of code and transition
+lines(mass_Gt ~ decimal_date, data = grn_ice_loss_NA, type='l', col='red')
 
-# start 1/29 code - still need to go back and fill in gaps
+head(ant_ice_loss_NA)
 
-# n(ant_ice_loss$mass_Gt)
-# min(grn_ice_loss
+# NASA gave sigma (1 std dev) which represents 68% of uncertainty
+# in the header, they call it "1-sigma", meaning '1 std', and NOT '1 minus the std'
+# sometimes we show error as "2-sigma", where 2 X sigma represents 95% uncertainty
+# we can plot 95% confidence intervals around the data by adding and subtracting 2*sigma from the mass loss estimates
 
+plot(mass_Gt ~ decimal_date, data = ant_ice_loss_NA, ylab='Antarctica Mass Loss (Gt)', xlab='Year',
+type='l', lwd=2) +
+lines((mass_Gt+2*sigma_Gt) ~ decimal_date, data = ant_ice_loss_NA, type='l', lty='dashed') +
+lines((mass_Gt-2*sigma_Gt) ~ decimal_date, data = ant_ice_loss_NA, type='l', lty='dashed')
 
-# lines(
- #   mass_Gt ~ decimal_date
-# )
+# combine Ant and Grn time series plots into the same plot and save plot to hard drive
+pdf('unit_2_climate/figures/ice_mass_trends.pdf', width = 7, height = 5)
+plot(mass_Gt ~ decimal_date, data = ant_ice_loss_NA, ylab = "Ice Sheet Mass Loss (Gt)",
+    xlab="Year", type='l', ylim=range(grn_ice_loss_NA$mass_Gt, na.rm=TRUE), lwd=2) +
+lines((mass_Gt+2*sigma_Gt) ~ decimal_date, data = ant_ice_loss_NA, type='l', lty='dashed') +
+lines((mass_Gt-2*sigma_Gt) ~ decimal_date, data = ant_ice_loss_NA, type='l', lty='dashed') +
+lines((mass_Gt ~ decimal_date), data=grn_ice_loss_NA, type='l', lwd=2) +
+lines((mass_Gt+2*sigma_Gt) ~ decimal_date, data=grn_ice_loss_NA, type='l', lty='dashed', col='red') +
+lines((mass_Gt-2*sigma_Gt) ~ decimal_date, data=grn_ice_loss_NA, type='l', lty='dashed', col='red')
+dev.off()
 
+# make a bar plot
+## largest observed decrease in ice mass loss in Antarctica
+min(ant_ice_loss$mass_Gt)
+
+# barplot of largest observed ice loss in Antarctica and Greenland
+barplot(height=c(min(ant_ice_loss$mass_Gt), min(grn_ice_loss$mass_Gt)))
+
+#flip to negative to positive, add x-axis labels, add more tick marks on y-axis, add y-axis title
+barplot(height= c(min(ant_ice_loss$mass_Gt)*(-1), min(grn_ice_loss$mass_Gt)*(-1)), 
+        names.arg=c("Antarctica", "Greenland"), ylim=c(0,6000), ylab="Ice loss in Gt")
